3f9eab24fdc38af9c85878bc5132c99618e34076
greytHRacademy <ghr_academy@greytip.com>
2026-04-10 17:31:02 +0530

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,32 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xomaz9dbpr":
+        Script1();
+        break;
+      case "66SNyX0siaY":
+        Script2();
+        break;
+      case "5yZU2UDxrwc":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
